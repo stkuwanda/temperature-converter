1c584147987bcc4dbc7217ff48257f64ca2a4d74
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-27 22:19:30 +0200

    added scripts for linting and testing to package.json file

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // convert fahrenheit to celsius
-function fahrenheitToCelsius(fahrenheit) {
-	return ((fahrenheit - 32) * 5) / 9;
+function fahrenheitToCelsius (fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
 }
 
 // convert celsius to fahrenheit
-function celsiusToFahrenheit(celsius) {
-	return (celsius * 9) / 5 + 32;
+function celsiusToFahrenheit (celsius) {
+  return (celsius * 9) / 5 + 32;
 }
 
 module.exports = { fahrenheitToCelsius, celsiusToFahrenheit };
 
 // $ node --print "require('./').fahrenheitToCelsius(100)"
 // $ node --print "require('./').celsiusToFahrenheit(37.7778)"
+
+// $ npm run lint --> test for code formatting issues
+// $ npm run lint -- --fix --> fix code formatting issues automatically
